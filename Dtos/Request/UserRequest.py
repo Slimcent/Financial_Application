ad8167a43c8363ada825f3dae11c90b3f5d73428
Slimcent <centpresley@gmail.com>
2025-03-28 11:32:06 +0100

    added utility module for password hashing

--- Dtos/Request/UserRequest.py
+++ Dtos/Request/UserRequest.py
@@ -1,5 +1,7 @@
 class UserRequest:
-    def __init__(self, name: str, email: str, role_id: int):
-        self.name = name
+    def __init__(self, last_name: str, first_name: str,  email: str, role_id: int, password: int):
+        self.last_name = last_name
+        self.first_name = first_name
         self.email = email
         self.role_id = role_id
+        self.password = password
--- Service/UserService.py
+++ Service/UserService.py
@@ -1,3 +1,4 @@
+from aiomysql import DictCursor
 from Dtos.Request.UserRequest import UserRequest
 
 
@@ -5,34 +6,36 @@ class UserService:
     def __init__(self, database_connection):
         self.database_connection = database_connection
 
-    def create_user(self, user_request: UserRequest):
-        connection = self.database_connection.connect()
+    async def create_user(self, user_request: UserRequest):
+        connection = await self.database_connection.get_connection()
         if not connection:
             return None
 
         try:
-            cursor = connection.cursor(dictionary=True)
+            async with connection.cursor(DictCursor) as cursor:
+                # Check if the user already exists
+                check_query = "SELECT Id FROM Users WHERE Email = %s"
+                await cursor.execute(check_query, (user_request.email,))
+                existing_user = await cursor.fetchone()
 
-            # Check if the user already exists
-            check_query = "SELECT Id FROM Users WHERE Email = %s"
-            cursor.execute(check_query, (user_request.email,))
-            existing_user = cursor.fetchone()
+                if existing_user:
+                    print("User with this email already exists")
+                    return None  # Return None if user exists
 
-            if existing_user:
-                print("User with this email already exists")
-                return None  # Return None if user exists
+                # Insert new user
+                insert_query = "INSERT INTO Users (LastName, FirstName, Email, RoleId, Password) VALUES (%s, %s, %s, " \
+                               "%s, %s) "
+                await cursor.execute(insert_query, (
+                    user_request.last_name, user_request.first_name, user_request.email, user_request.role_id,
+                    user_request.password))
+                await connection.commit()
 
-            insert_query = "INSERT INTO Users (Name, Email, RoleId) VALUES (%s, %s, %s)"
-            cursor.execute(insert_query, (user_request.name, user_request.email, user_request.role_id))
-            connection.commit()
-
-            return cursor.lastrowid
+                return cursor.lastrowid
 
         except Exception as e:
             print("Error creating user:", e)
-            connection.rollback()
+            await connection.rollback()
             return None
 
         finally:
-            cursor.close()
-            self.database_connection.close()
+            await self.database_connection.release_connection(connection)
